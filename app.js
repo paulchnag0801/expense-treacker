798e98ad489786f2d00cc8fb94cdf839ce28ed34
paulchnag0801 <x1232178987@gmail.com>
2021-07-23 12:38:45 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
 const app = express()
+
+
+
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
 
 app.get('/', (req, res) => {
   res.send('hello world')
