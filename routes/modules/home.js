d7841388c28287f9e7b6382bc45a44b6ebe6f6a5
paulchnag0801 <x1232178987@gmail.com>
2021-07-28 01:42:10 +0800

    fix Cap again

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const Record = require('../../models/record')
-const Category = require('../../models/category')
+const Record = require('../../models/Record')
+const Category = require('../../models/Category')
 // 設定首頁路由
 
 router.get('/', async (req, res) => {
