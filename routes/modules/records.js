5ebdc9fedcae1e2bb33c73a32e1062cb0b8287b1
paulchnag0801 <x1232178987@gmail.com>
2021-07-27 17:01:42 +0800

    fix: CRUD record pages route

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
-
+const Category = require('../../models/category')
 //create new record
-router.get('/new', (req, res) => {
-  res.render('new')
+router.get('/new', async (req, res) => {
+  const categoryList = await Category.find().sort({ _id: 'asc' }).lean()
+  res.render('new', { categoryList })
 })
+
 router.post('/', (req, res) => {
-  const { name, amount, category, date } = req.body
-  return Record.create({ name, amount, category, date })
+  const { name, date, category, amount, shop } = req.body
+  Record.create({ name, date, category, amount, shop })
     .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error))
 })
 
 // 瀏覽特定支出
@@ -42,37 +44,38 @@ router.get('/:id', (req, res) => {
 })
 
 //從主頁修改單筆支出
-router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  Record.findById(id)
+router.get('/:record_id/edit', async (req, res) => {
+  const categoryList = await Category.find().sort({ _id: 'asc' }).lean()
+  const id = req.params.record_id
+  return Record.findById(id)
     .lean()
-    .then((record) => {
-      res.render('edit', { record })
-    })
+    .then((record) => res.render('edit', { record, categoryList }))
+    .catch((error) => console.log(error))
 })
 //在修改頁面edit，編輯支出。
-router.put('/:id', (req, res) => {
-  const id = req.params.id
-  const { name, amount, category, date } = req.body
+router.put('/:record_id', (req, res) => {
+  const id = req.params.record_id
+  const { name, date, category, amount, shop } = req.body
   return Record.findById(id)
-    .then(record => {
+    .then((record) => {
       record.name = name
-      record.amount = amount
-      record.category = category
       record.date = date
+      record.category = category
+      record.amount = amount
+      record.shop = shop
       return record.save()
     })
-    .then(() => res.redirect(`/records/${id}`))
-    .catch(error => console.log(error))
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error))
 })
 
 // 刪除單筆支出
-router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  Record.findById(id)
-    .then(record => record.remove())
+router.delete('/:record_id', (req, res) => {
+  const id = req.params.record_id
+  return Record.findById(id)
+    .then((record) => record.remove())
     .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error))
 })
 
 module.exports = router
