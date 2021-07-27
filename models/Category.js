368b4152ad5474ad672087d3d35c1aa5a530da49
paulchnag0801 <x1232178987@gmail.com>
2021-07-27 17:12:41 +0800

    feat : add categorySchema

---
+++ models/Category.js
@@ -0,0 +1,15 @@
+// mongoose
+const mongoose = require('mongoose') // 載入 mongoose
+const Schema = mongoose.Schema
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('Category', categorySchema)
