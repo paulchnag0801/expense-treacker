518b699d04ed31736515aef4c51b56261ec5c415
paulchnag0801 <x1232178987@gmail.com>
2021-07-27 17:15:29 +0800

    refactor:  add hbs-helper connection & PORT

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
 const routes = require('./routes')
+const { ifEqual } = require('./tools/handlebarshelpers')
 const app = express()
-const Record = require('./models/record')
 const methodOverride = require('method-override')
 const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: '.hbs',
-  helpers: {
-    eq: function (a, b) {
-      return a === b
-    },
-  },
+  helpers: { ifEqual } ,
 })
-mongoose.connect('mongodb://localhost/expense-tracker', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}) // 設定連線到 mongoDB
+const PORT = process.env.PORT || 3000
+require('./config/mongoose')
 
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
@@ -38,11 +21,8 @@ app.use(express.static('public'))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-//宣告篩選category
-let filteredCategory = ''
 
 
-
-app.listen(3000, () => {
-  console.log('App is running in http;//localhost:3000')
+app.listen(PORT, () => {
+  console.log(`App is running in http;//localhost:${PORT}`)
 })
