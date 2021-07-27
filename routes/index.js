18db394ba76909f5ce63eb9716115b1c383b832c
paulchnag0801 <x1232178987@gmail.com>
2021-07-27 17:07:30 +0800

    refactor:  home & records router

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
-const filter = require('./modules/filter')
 const records = require('./modules/records')
 
-router.use('/', home) // 將網址結構符合 / 字串開頭的 request 導向 home 模組
-router.use('/filter', filter)  // 將網址結構符合 /filter 字串開頭的 request 導向 filter 模組
-router.use('/records', records)//將網址結構符合 /records 字串開頭的 request 導向 records 模組
-
+router.use('/', home) // 將網址結構符合 / 導入home.js
+router.use('/records',records) //將網址結構符合 /records 導入 records.js
 module.exports = router
