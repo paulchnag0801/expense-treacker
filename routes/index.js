cbc81e35bd2d3af276b305ae3641060028a25154
paulchnag0801 <x1232178987@gmail.com>
2021-10-18 16:27:28 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const records = require('./modules/records')
+const users = require('./modules/users')
 
 router.use('/', home) // 將網址結構符合 / 導入home.js
 router.use('/records',records) //將網址結構符合 /records 導入 records.js
+router.use('/users', users)
 module.exports = router
