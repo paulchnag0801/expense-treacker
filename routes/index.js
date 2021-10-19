89dce993dcc7a212b6f8c854a6aacf2322805c3a
paulchnag0801 <x1232178987@gmail.com>
2021-10-19 17:46:28 +0800

    feat: add user authentication in records and home routers

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ const home = require('./modules/home')
 const records = require('./modules/records')
 const users = require('./modules/users')
 
-router.use('/', home) // 將網址結構符合 / 導入home.js
-router.use('/records',records) //將網址結構符合 /records 導入 records.js
+const { authenticator } = require('../middleware/auth')
+router.use('/records', authenticator,records) //將網址結構符合 /records 導入 records.js
 router.use('/users', users)
+router.use('/', authenticator, home) // 將網址結構符合 / 導入home.js
 module.exports = router
