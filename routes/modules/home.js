dd2ac6e07c0d742cb4eb46065ac5a449d45e4e95
paulchnag0801 <x1232178987@gmail.com>
2021-10-27 18:20:52 +0800

    feat : fix fitler change userid to records

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -32,9 +32,10 @@ router.get('/filter', async (req, res) => {
   if (categorySelector === 'All') {
     return res.redirect('/')
   }
-    const records = await Record.find({ category: categorySelector })
-      .lean()
-      .sort({ _id: 'desc' })
+  const userId = req.user._id
+  const records = await Record.find({ category: categorySelector, userId })
+    .lean()
+    .sort({ _id: 'desc' })
   let totalAmount = 0
   for (let record of records) {
     totalAmount += record.amount
